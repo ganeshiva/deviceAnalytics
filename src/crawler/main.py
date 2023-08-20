44a4ced0ac5ff06ae9f5eec75b4993b3fa9c25c8
ganeshiva <10473065+ganeshiva@users.noreply.github.com>
2023-08-20 18:27:41 +0530

    Create main.py
    First Commit with just the header

---
+++ src/crawler/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+codeHeader='''
+#################################################################
+title="deviceAnalytics/Crawler"
+summary="Main code for crawling data from device"
+author=Ganeshiva
+created=20230820
+updated=20230820
+cmdLine="python3 <thisScriptName> <configFile>"
+dependancy="refer requirement.txt"
+repository="https://github.com/ganeshiva/deviceAnalytics"
+license="refer https://github.com/ganeshiva/deviceAnalytics/blob/main/LICENSE"
+#################################################################
+'''
+
+print(codeHeader)
